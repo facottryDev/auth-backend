3b943c22be71ca1be72c8bb788c31be2576f9a7a
Styles <bcs_2021035@iiitm.ac.in>
2024-02-04 19:24:38 +0530

    Update router.js

--- router.js
+++ router.js
@@ -4,14 +4,14 @@ import { Router } from "express";
 const router = Router();
 
 //AUTH
-router.post("/auth/login", loginUser);
-router.get("/auth/logout", logOut);
-router.post("/auth/is-registered", isRegistered);
-router.post("/auth/send-otp", sendOTP);
-router.post("/auth/verify-otp", verifyOTP);
-router.post("/auth/register", registerUser);
-router.post("/auth/forgot", forgotPassword);
-router.post("/auth/reset", resetPassword);
+router.post("/login", loginUser);
+router.get("/logout", logOut);
+router.post("/is-registered", isRegistered);
+router.post("/send-otp", sendOTP);
+router.post("/verify-otp", verifyOTP);
+router.post("/register", registerUser);
+router.post("/forgot", forgotPassword);
+router.post("/reset", resetPassword);
 
 //USER
 router.get("/", isAuth, (req, res) => {
